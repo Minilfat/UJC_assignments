a4369d8bc33cb440119c160af7ae84cf45980743
Ilfat Mindubaev <garantid@mail.ru>
2018-02-11 20:20:22 +0300

    Main with functional approach

--- MultiThTask/src/edu/uni/Main.java
+++ MultiThTask/src/edu/uni/Main.java
@@ -6,89 +6,100 @@ import edu.uni.resources.FileResourceA;
 
 import edu.uni.resources.IResource;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 
 public class Main {
 
 
     private static String[] fileNames = {"files/1.csv", "files/2.csv", "files/4.csv","files/3.csv"};
     // у ресурхандлера есть поле ресурс, который имплементит IResource
-    private static ResourceHandler[] pool = new ResourceHandler[fileNames.length];
+    private static List<ResourceHandler> pool = new ArrayList<>(fileNames.length);
 
-
-    // мы создаем и запускаем потоки
-    // которые с файловым ресурсом А
-    // в рантайме хотим поменять его на файловым ресурс FileResourceB
-    private static void go() {
-        for (int i=0; i<pool.length; i++) {
-            pool[i] = new ResourceHandler(new FileResourceA(fileNames[i]));
-            new Thread(pool[i]).start();
-        }
-    }
+    private static Class<?> newFileResource;
 
 
     public static void main(String[] args) {
 
+        // создаем трэды с ресурсхэндлерами и добавляем в пулл
+        Arrays.stream(fileNames)
+                .map(FileResourceA::new)
+                .map(ResourceHandler::new)
+                .forEach(pool::add);
 
-
-        // старутем трэды по подсчету суммы
-        go();
+        // заускаем треды
+        pool.stream()
+                .map(Thread::new)
+                .forEach(Thread::start);
 
         // новый трэд за мониторингом условия смены логики суммирования
-        new Thread(() -> {
-            while (true) {
-                // при добавлении суммы синхронизириемся также по Sum.class
-                // (см.  классы имплементирующие IResource)
-                synchronized (Sum.class) {
-                    // здесь условие смены Iresource в классе ResourceHandler
-                    if (Sum.value !=0 && Sum.value % 2 == 0) {
-                        if (!change())
-                            System.out.println("Couldn't change classes in runtime");
-                        break;
-                    }
-                }
-            }
-        }).start();
+        new Thread(Main::monitor).start();
 
 
 
 
     }
 
+    private static void monitor() {
+        while (true) {
+            // при добавлении суммы синхронизириемся также по Sum.class
+            // (см.  классы имплементирующие IResource)
+            synchronized (Sum.class) {
+                // здесь условие смены Iresource в классе ResourceHandler
+                if (Sum.value != 0 && Sum.value % 2 == 0) {
+                    if (!change())
+                        System.out.println("Couldn't change classes in runtime");
+                    else
+                        System.out.println("*************** смена логики суммирования ***************");
+                    break;
+                }
+            }
+        }
+    }
 
 
     private static boolean change() {
-        Class<?> clazz = null;
+        newFileResource= null;
         try {
-            clazz = new MyLoader().loadClass("edu.uni.resources.FileResourceB");
+            newFileResource = new MyLoader().loadClass("edu.uni.resources.FileResourceB");
         } catch (ClassNotFoundException e) {
             System.out.println("Class was not found...");
             return false;
         }
 
-        for (int i=0; i<pool.length; i++) {
-            IResource newRes = null;
-            try {
-                newRes = (IResource) clazz.newInstance();
-            } catch (InstantiationException e) {
-                e.printStackTrace();
-                System.out.println("Couldn't initialize a class");
-            } catch (IllegalAccessException e) {
-                e.printStackTrace();
-                System.out.println("Это я хз вообще как обрабатывать");
-            }
+        List<IResource> list = pool.stream()
+                .map(s -> initLoadedResource())
+                .collect(Collectors.toList());
 
-            if (newRes == null)
-                return false;
-
-
-            exchangeFields(pool[i].getRes(), newRes);
-            pool[i].setRes(newRes);
-
+        for (int i = 0; i<pool.size(); i++) {
+            IResource tmp = list.get(i);
+            exchangeFields(pool.get(i).getRes(), tmp);
+            pool.get(i).setRes(tmp);
         }
+
+
         return true;
 
     }
 
+    private static IResource initLoadedResource() {
+        IResource newRes;
+        try {
+            newRes = (IResource) newFileResource.newInstance();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+            System.out.println("Couldn't initialize a class");
+            return null;
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+            System.out.println("Это я хз вообще как обрабатывать");
+            return null;
+        }
+        return newRes;
+    }
 
 
     // Resource содержит много полей
