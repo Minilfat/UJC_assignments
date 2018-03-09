341033eef7c3bcebb79e3dc63d2315e5d7b148f9
Ilfat Mindubaev <garantid@mail.ru>
2018-03-09 06:14:02 +0300

    Initial commit. Simple web service with Vert.x

---
+++ Cservice/src/main/java/edu/service/MainVerticle.java
@@ -0,0 +1,81 @@
+package edu.service;
+
+import edu.service.models.ClientProfile;
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
+import io.vertx.core.http.HttpHeaders;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
+import io.vertx.ext.web.handler.BodyHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import sun.plugin.util.UserProfile;
+
+public class MainVerticle extends AbstractVerticle {
+
+    private static Logger LOG = LoggerFactory.getLogger(MainVerticle.class);
+
+    @Override
+    public void start(Future<Void> startFuture) throws Exception {
+
+
+        Router httpRouter = Router.router(vertx);
+        httpRouter.route().handler(BodyHandler.create());
+
+        // Get requests
+        httpRouter.get("/captcha/new").handler(this::newCaptcha);
+        httpRouter.get("/captcha/image").handler(this::getImage);
+        httpRouter.get("/captcha/verify").handler(this::verify);
+
+        // Post requests
+        httpRouter.post("/register/new/").handler(this::newUser);
+        httpRouter.post("/captcha/solve").handler(this::checkAnswer);
+
+
+        vertx.createHttpServer()
+                .requestHandler(httpRouter::accept)
+                .listen(8080, i -> {
+                    if (i.succeeded())
+                        LOG.info("Server started on port: " + 8080);
+                    else
+                        LOG.error("Couldn't start a server: ", i.cause());
+                });
+    }
+
+
+
+    private void newCaptcha(RoutingContext context) {
+
+    }
+
+    private void getImage(RoutingContext context) {
+    }
+
+    private void verify(RoutingContext routingContext) {
+    }
+
+
+
+
+    private void newUser(RoutingContext context) {
+        ClientProfile c = new ClientProfile();
+        JsonObject json = new JsonObject().put("secret", c.getPrivateKey().toString())
+                                          .put("public", c.getPublicKey().toString());
+
+        context.response().putHeader(HttpHeaders.CONTENT_TYPE, "application/json")
+                     .end(json.encode());
+
+        LOG.info("New client connected: " + c.getPrivateKey() + ", " +  c.getPublicKey());
+
+    }
+
+
+
+    private void checkAnswer(RoutingContext routingContext) {
+    }
+
+
+
+
+}
---
+++ Cservice/src/main/java/edu/service/models/ClientProfile.java
@@ -0,0 +1,36 @@
+package edu.service.models;
+
+import java.util.UUID;
+
+public class ClientProfile {
+
+    private UUID privateKey;
+    private UUID publicKey;
+
+
+    public ClientProfile() {
+        this.privateKey = UUID.randomUUID();
+        this.publicKey = UUID.randomUUID();
+    }
+
+    public ClientProfile(UUID privateKey, UUID publicKey) {
+        this.privateKey = privateKey;
+        this.publicKey = publicKey;
+    }
+
+    public UUID getPrivateKey() {
+        return privateKey;
+    }
+
+    public void setPrivateKey(UUID privateKey) {
+        this.privateKey = privateKey;
+    }
+
+    public UUID getPublicKey() {
+        return publicKey;
+    }
+
+    public void setPublicKey(UUID publicKey) {
+        this.publicKey = publicKey;
+    }
+}
